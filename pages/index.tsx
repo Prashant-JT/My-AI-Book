9722d6d21f0f9e7a4719f776b8227732f4449931
Prashant-JT <prashantjeswani8@gmail.com>
2022-03-12 04:15:05 +0100

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Header from '../components/Header';
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>My AI Book</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      {/* POSTS */}
+      
+    </div>
+  );
+}
