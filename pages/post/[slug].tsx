732e23b3d01bb0f106eedd212bd43fd5afef40b3
Prashant-JT <prashantjeswani8@gmail.com>
2022-03-12 19:51:46 +0100

    Fetch post body

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,6 +2,7 @@ import { GetStaticProps } from 'next';
 import Header from '../../components/Header';
 import {sanityClient, urlFor} from '../../sanity'
 import {Post} from '../../typings'
+import PortableText from 'react-portable-text'
 
 interface Props {
     post: Post
@@ -25,7 +26,29 @@ function Post({post}: Props) {
               {new Date(post._createdAt).toLocaleString()}</p>
           </div>
 
-          
+          <div className='mt-10'>
+              <PortableText 
+              dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+              projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+              content={post.body}
+              serializers={{
+                h1: (props: any) => (
+                    <h1 className='text-2xl font-bold my-5' {...props}/>
+                ),
+                h2: (props: any) => (
+                    <h1 className='text-xl font-bold my-5' {...props}/>
+                ),
+                li: ({children}: any) => (
+                    <li className='ml-4 list-disc'>{children}</li>
+                ),
+                link: ({href, children}: any) => (
+                    <a href={href} 
+                    className='text-blue-500 hover:underline'>{children}</a>
+                ),
+              }}
+              />
+          </div>
+
       </article>
   </main>;
 }
